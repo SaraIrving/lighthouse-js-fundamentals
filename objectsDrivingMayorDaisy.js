01c17baeb28cfe8076b46cc5bb15a713f3ba6609
Sara Irving <sarakirving@gmail.com>
2020-05-29 18:08:55 +0000

    Objects Driving Mayor Daisy Assignment

---
+++ objectsDrivingMayorDaisy.js
@@ -0,0 +1,20 @@
+function carPassing (cars, speed) {
+  cars.push({time: Date.now(), speed: speed});
+  return cars; 
+}
+
+console.log(carPassing([
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+, 38));
